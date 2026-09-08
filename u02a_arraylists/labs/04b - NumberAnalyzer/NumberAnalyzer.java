1359d2036717ba425f75fd10259bcadd19a25370
ap095-droid <andrewpena640@gmail.com>
2026-09-07 19:45:14 -0500

    Update NumberAnalyzer.java
    transport over finished lab from Google drive

--- u02a_arraylists/labs/04b - NumberAnalyzer/NumberAnalyzer.java
+++ u02a_arraylists/labs/04b - NumberAnalyzer/NumberAnalyzer.java
@@ -14,50 +14,68 @@ public class NumberAnalyzer
 
 	public NumberAnalyzer()
 	{
-
+		list = new ArrayList<Number>();
 	}
 
 	public NumberAnalyzer(String numbers)
 	{
-
+		list = new ArrayList<Number>();
+		setList(numbers);
 	}
 	
 	public void setList(String numbers)
 	{
+		list.clear();
 
+		Scanner scan = new Scanner(numbers);
 
-	
+		while(scan.hasNextInt())
+		{
+			list.add(new Number(scan.nextInt()));
+		}
 	}
 
 	public int countOdds()
 	{
-      int oddCount=0;
+      int oddCount = 0;
 
-
+      for(Number num : list)
+      {
+      	if(num.isOdd())
+      		oddCount++;
+      }
 
       return oddCount;
 	}
 
 	public int countEvens()
 	{
-      int evenCount=0;
+      int evenCount = 0;
 
-
+      for(Number num : list)
+      {
+      	if(!num.isOdd())
+      		evenCount++;
+      }
 
       return evenCount;
 	}
 
 	public int countPerfects()
 	{
-		int perfectCount=0;
+		int perfectCount = 0;
 
-
+		for(Number num : list)
+		{
+			if(num.isPerfect())
+				perfectCount++;
+		}
 
       return perfectCount;
 	}
 	
 	public String toString( )
 	{
-		return "";
+		return list.toString();
 	}
 }
