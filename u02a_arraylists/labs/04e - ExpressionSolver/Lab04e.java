2a9408210cde3e9ee89ea88f6b770d89e204d405
ap095-droid <andrewpena640@gmail.com>
2026-09-07 20:02:57 -0500

    Update Lab04e.java
    transport finished lab from Google drive

--- u02a_arraylists/labs/04e - ExpressionSolver/Lab04e.java
+++ u02a_arraylists/labs/04e - ExpressionSolver/Lab04e.java
@@ -4,14 +4,25 @@
 //Class -
 //Lab  -
 
-import java.util.ArrayList;
 import java.util.Scanner;
+import java.io.File;
 import static java.lang.Integer.*;
 import static java.lang.System.*;
 
 public class Lab04e
 {
-	public static void main( String args[] )
+	public static void main( String args[] ) throws Exception
 	{
+		Scanner file = new Scanner(new File("lab04e.dat"));
+
+		while(file.hasNextLine())
+		{
+			String exp = file.nextLine();
+
+			ExpressionSolver test = new ExpressionSolver(exp);
+			test.solveExpression();
+
+			out.println(test);
+		}
 	}
 }
