bcd07e75822c4eeec5bbf1814a8a5cb72a2eb3c0
ap095-droid <andrewpena640@gmail.com>
2026-09-07 19:42:53 -0500

    Update Number.java
    transport finished lab from Google drive

--- u02a_arraylists/labs/04b - NumberAnalyzer/Number.java
+++ u02a_arraylists/labs/04b - NumberAnalyzer/Number.java
@@ -10,44 +10,46 @@ public class Number
 
 	public Number()
 	{
-
-
+		number = 0;
 	}
 
 	public Number(int num)
 	{
-
-
+		number = num;
 	}
 	
 	public void setNumber(int num)
 	{
-
-
+		number = num;
 	}
 	
 	public int getNumber()
 	{
-		return 0;
+		return number;
 	}	
 	
 	public boolean isOdd()
 	{
-		return false;
+		return number % 2 == 1;
 	}
 	
 	public boolean isPerfect()
 	{
-		int total=0;
+		int total = 0;
 
+		for(int i = 1; i < number; i++)
+		{
+			if(number % i == 0)
+				total += i;
+		}
 
-
-
-		return (number==total);
+		return (number == total);
 	}	
 	
 	public String toString( )
 	{
-		return "";
+		return "" + number
+			+ "\n" + number + " isOdd == " + isOdd()
+			+ "\n" + number + " isPerfect == " + isPerfect();
 	}
 }
