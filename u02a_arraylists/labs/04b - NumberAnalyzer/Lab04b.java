f76cbc898f87f404e86504ad84109db03ddf025f
ap095-droid <andrewpena640@gmail.com>
2026-09-07 19:45:56 -0500

    Update Lab04b.java
    transport fixed lab from Google drive

--- u02a_arraylists/labs/04b - NumberAnalyzer/Lab04b.java
+++ u02a_arraylists/labs/04b - NumberAnalyzer/Lab04b.java
@@ -14,13 +14,20 @@ public class Lab04b
 	{
 		NumberAnalyzer test = new NumberAnalyzer("5 12 9 6 1 4 8 6");
 		out.println(test);
-		out.println("odd count = "+test.countOdds());
-		out.println("even count = "+test.countEvens());
-		out.println("perfect count = "+test.countPerfects()+"\n\n\n");
+		out.println("odd count = " + test.countOdds());
+		out.println("even count = " + test.countEvens());
+		out.println("perfect count = " + test.countPerfects() + "\n\n");
 
+		test = new NumberAnalyzer("5 12 3 7 28 496 81 65 33 11");
+		out.println(test);
+		out.println("odd count = " + test.countOdds());
+		out.println("even count = " + test.countEvens());
+		out.println("perfect count = " + test.countPerfects() + "\n\n");
 
-		//add more test cases
-		
-			
+		test = new NumberAnalyzer("1 2 3 4 5 6 7 8 11 13 151 16 17 18 19 20");
+		out.println(test);
+		out.println("odd count = " + test.countOdds());
+		out.println("even count = " + test.countEvens());
+		out.println("perfect count = " + test.countPerfects());
 	}
 }
