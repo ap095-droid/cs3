72e2367836bad65fa103cce1ffffd1e12eabcb1f
ap095-droid <andrewpena640@gmail.com>
2026-09-07 20:02:23 -0500

    Update ExpressionSolver.java
    transport finsihed lab from google drive

--- u02a_arraylists/labs/04e - ExpressionSolver/ExpressionSolver.java
+++ u02a_arraylists/labs/04e - ExpressionSolver/ExpressionSolver.java
@@ -11,23 +11,128 @@ import static java.lang.System.*;
 
 public class ExpressionSolver
 {
-	String origExpr;
-   String 
+	private String expression;
+	private int answer;
 
 	public ExpressionSolver(String s)
 	{
+		setExpression(s);
 	}
 
 	public void setExpression(String s)
 	{
+		expression = s.replaceAll("\\s+", "");
+		answer = 0;
 	}
 
 	public void solveExpression()
 	{
+		answer = solve(expression);
 	}
 
-	public String toString( )
+	private int solve(String exp)
 	{
-		return "";
+		
+		while(exp.indexOf("(") != -1)
+		{
+			int close = exp.indexOf(")");
+			int open = exp.lastIndexOf("(", close);
+
+			String inside = exp.substring(open + 1, close);
+			int value = solve(inside);
+
+			exp = exp.substring(0, open) + value + exp.substring(close + 1);
+		}
+
+		ArrayList<String> list = new ArrayList<String>();
+
+		String num = "";
+		for(int i = 0; i < exp.length(); i++)
+		{
+			char ch = exp.charAt(i);
+
+			if(Character.isDigit(ch))
+			{
+				num += ch;
+			}
+			else
+			{
+				list.add(num);
+				list.add("" + ch);
+				num = "";
+			}
+		}
+		list.add(num);
+
+		
+		for(int i = 1; i < list.size(); i += 2)
+		{
+			if(list.get(i).equals("^"))
+			{
+				int a = Integer.parseInt(list.get(i - 1));
+				int b = Integer.parseInt(list.get(i + 1));
+
+				int value = (int)Math.pow(a, b);
+
+				list.remove(i - 1);
+				list.remove(i - 1);
+				list.set(i - 1, "" + value);
+
+				i -= 2;
+			}
+		}
+
+		
+		for(int i = 1; i < list.size(); i += 2)
+		{
+			if(list.get(i).equals("*") || list.get(i).equals("/"))
+			{
+				int a = Integer.parseInt(list.get(i - 1));
+				int b = Integer.parseInt(list.get(i + 1));
+
+				int value;
+
+				if(list.get(i).equals("*"))
+					value = a * b;
+				else
+					value = a / b;
+
+				list.remove(i - 1);
+				list.remove(i - 1);
+				list.set(i - 1, "" + value);
+
+				i -= 2;
+			}
+		}
+
+		
+		for(int i = 1; i < list.size(); i += 2)
+		{
+			if(list.get(i).equals("+") || list.get(i).equals("-"))
+			{
+				int a = Integer.parseInt(list.get(i - 1));
+				int b = Integer.parseInt(list.get(i + 1));
+
+				int value;
+
+				if(list.get(i).equals("+"))
+					value = a + b;
+				else
+					value = a - b;
+
+				list.remove(i - 1);
+				list.remove(i - 1);
+				list.set(i - 1, "" + value);
+
+				i -= 2;
+			}
+		}
+
+		return Integer.parseInt(list.get(0));
+	}
+
+	public String toString()
+	{
+		return expression + " = " + answer;
 	}
 }
