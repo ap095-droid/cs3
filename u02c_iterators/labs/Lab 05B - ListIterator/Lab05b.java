9813a20561f6386eac5dca96dddd197f1d7c091f
ap095-droid <andrewpena640@gmail.com>
2026-09-14 07:59:51 -0500

    Update Lab05b.java
    copied over edited lab file from Google drive

--- u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
+++ u02c_iterators/labs/Lab 05B - ListIterator/Lab05b.java
@@ -1,18 +1,23 @@
-//© A+ Computer Science  -  www.apluscompsci.com
-//Name -
-//Date -
-//Class -
-//Lab  -
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.ListIterator;
+
 import static java.lang.System.*;
 
 public class Lab05b
 {
-	public static void main ( String[] args )
+	public static void main(String[] args)
 	{
-		//add test cases		
+		ListIteratorTest test;
+
+		test = new ListIteratorTest("a b c a b c a +");
+		test.replace("a", "+");
+		out.println(test);
+
+		test = new ListIteratorTest("a b c d e f g h i j x x x x x 7");
+		test.replace("x", "7");
+		out.println(test);
+
+		test = new ListIteratorTest("1 2 3 4 5 6 a b c a b c b #");
+		test.replace("b", "#");
+		out.println(test);
 	}
 }
